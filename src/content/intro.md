45f620078b27fdbb10ca997d4066b187da9b1522
Stefanos <stefanosu422@gmail.com>
2020-03-27 18:10:24 -0400

    added markdown files

---
+++ src/content/intro.md
@@ -0,0 +1,6 @@
+ ---
+ section: intro
+ title: Hi there!
+ description: I’m Stefanos and I’m a Software engineer!
+ action: Hire me
+ ---
---
+++ src/data/index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  defaultTitle: "Stefanos",
+  logo: "https://gatsby-theme-portfolio.smakosh.com/favicon/favicon-512.png",
+  author: "Stefanos Ugbit",
+  url: "https://gatsby-theme-portfolio.smakosh.com",
+  legalName: "Stefanos Ugbit",
+  defaultDescription: "I’m Stefanos and I’m a Software engineer!",
+  socialLinks: {
+    twitter: "https://twitter.com/StefanosUgbit",
+    github: "https://github.com/stefanosu",
+    linkedin: "https://www.linkedin.com/in/stefanos-ugbit-a8324b40/",
+    medium: "https://medium.com/@stefanosu422"
+  },
+  googleAnalyticsID: "UA-xxxxx-x",
+  themeColor: "#6b63ff",
+  backgroundColor: "#6b63ff",
+  social: {
+    facebook: "appId",
+    twitter: "@smakosh"
+  },
+  address: {
+    city: "City",
+    region: "Region",
+    country: "Country",
+    zipCode: "ZipCode"
+  },
+  contact: {
+    email: "email",
+    phone: "phone number"
+  },
+  foundingDate: "2019",
+  recaptcha_key: "xxxxxxxxxx"
+};
