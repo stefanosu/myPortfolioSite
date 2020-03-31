a22e06dd9cd11759d91d693185a6c8e31b00fc6d
Stefanos <stefanosu422@gmail.com>
2020-03-30 21:13:13 -0400

    added more gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,27 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     }, 
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-less`,
+      options: {
+        javascriptEnabled: true,
+      }
+    },
+    {
+      resolve: `gatsby-source-cosmicjs`,
+      options: {
+        bucketSlug: `my-portfolio-site`, // Get this value in Bucket > Settings
+        objectTypes: [`pages`],
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem', 
+      options: { 
+        name: `src`,
+        path: `${__dirname}/src/`,
+      }, 
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
