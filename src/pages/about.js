bb9ba1b405fd5ec056b2c995743e54d38bb22bd3
Stefanos <stefanosu422@gmail.com>
2020-04-02 16:50:06 -0400

    rendering queried data

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,25 +4,54 @@ import Container from '../components/container'
 import Layout from '../components/layout'
 import {graphql} from 'gatsby'
 
-export default ({ data }) => (
-<Layout> 
-  <Container> 
-    <div style={{color:`teal`}}>
-    <h1>{data.site.siteMetadata.title}</h1>
-    <p>React is cool!</p>
-    </div>
-    </Container>
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout> 
+      <Container> 
+        <div style={{color:`teal`}}>
+          <h1>My site's files</h1>
+            <p>React is cool!</p>
+              <table>
+                <thead>
+                  <tr> 
+                    <th>relativePath</th>
+                    <th>prettySize</th>
+                    <th>extension</th>
+                    <th>birthTime</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {data.allFile.edges.map(({ node }, index) => (
+                    <tr key={index}>
+                      <td>{node.relativePath}</td>
+                      <td>{node.prettySize}</td>
+                      <td>{node.extension}</td>
+                      <td>{node.birthTime}</td>
+                    </tr>
+                  ))}
+                  </tbody>
+            </table>
+        </div>
+      </Container>
     </Layout>  
-    )
+  )
+}
     
     
 
 
 export const query = graphql `
   query{
-    site {
-      siteMetadata {
-        title
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow:true) 
+        }
       }
     }
   }
