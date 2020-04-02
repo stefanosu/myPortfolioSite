e7151b44d99bb4bf61a89d9830998d9f4bf43549
Stefanos <stefanosu422@gmail.com>
2020-04-02 19:38:53 -0400

    exported createNode and createPages methods

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,9 +6,48 @@
 
 // You can delete this file if you're not using it
 
+const path = require(`path`)
+const {createFilePath} = require(`gatsby-source-filesystem`)
 
-exports.onCreateNode = ({ node }) => {
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions 
   if (node.internal.type === `MarkdownRemark`) {
-    console.log(node.internal.type)
+    const slug = (createFilePath({node, getNode, basePath:`pages`}))
+    createNodeField({
+      node,
+      name: `slug`, 
+      value: slug, 
+    })
   }
+}
+
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions 
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+    console.log(JSON.stringify(result, null, 4))
+
+
+result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  createPage({
+    path: node.fields.slug, 
+    component: path.resolve(`./src/templates/blog-post.js`), 
+    context: {
+      //data passed into context available in pageQueries as Graphql variables 
+    slug: node.fields.slug
+      },
+    })
+  })
 }
