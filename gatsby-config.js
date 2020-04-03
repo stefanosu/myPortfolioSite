3f2fc5cb772135cb920b2582771f5e8c52f4c578
Stefanos <stefanosu422@gmail.com>
2020-04-02 20:03:07 -0400

    added offline and manigest plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,9 +54,22 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-      `gatsby-transformer-remark`
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+      `gatsby-transformer-remark`,
+
+          {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: `GatsbyJS`,
+              short_name: `GatsbyJS`,
+              start_url: `/`,
+              background_color: `#6b37bf`,
+              theme_color: `#6b37bf`,
+              // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+              // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+              display: `standalone`,
+              icon: `src/images/icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        `gatsby-plugin-offline`,
+    ],
+  }
